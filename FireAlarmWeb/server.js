1736e67e19fc248ba54d5cc198ae7f83b71a9f58
Fauxx <156500552+Fauxx@users.noreply.github.com>
2025-09-18 16:23:07 +0800

    Update server.js
    to listen to all ports not just local

--- FireAlarmWeb/server.js
+++ FireAlarmWeb/server.js
@@ -73,6 +73,6 @@ app.post('/logout', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
